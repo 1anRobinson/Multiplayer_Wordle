8bc428f06aa2c86e84a4902adf9e1afffa2f51b6
DerekYeeter <alex.robi.2020@gmail.com>
2023-04-17 21:39:54 -0400

    implemented a start screen with new game and credit buttons. Just need to implement the actual game logic

--- wordle.js
+++ wordle.js
@@ -1,5 +1,11 @@
 const tileDisplay = document.querySelector(".tile-container")
 const keybord = document.querySelector(".key-container")
+const newGame = document.querySelector(".newGame button")
+newGame.addEventListener('click', gameStyle)
+const credits = document.querySelector(".credits button")
+credits.addEventListener('click', credit)
+const backButt = document.querySelector(".backButton")
+backButt.addEventListener('click', back)
 
 const keys = [
     'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
@@ -10,19 +16,55 @@ const onCLick = () =>
 {
     console.log("pressed.")
 }
+const menuDiv = document.querySelector(".start-container")
+const creditsDiv = document.querySelector(".credits-container")
+const backDiv = document.querySelector(".backButton")
+const typeDiv = document.querySelector(".type-container")
 
-for(let i = 0; i < 30; i++)
+
+function gameStyle()
 {
-    const buttElement = document.createElement('button')
-    buttElement.addEventListener('click', onCLick)
-    tileDisplay.append(buttElement) 
+    backDiv.style.display = 'block'
+    menuDiv.style.display = 'none'
+    typeDiv.style.display = 'block'
+    const atmps = document.querySelector(".type-container button")
+    atmps.addEventListener('click', start)
 }
 
-keys.forEach(key => 
+function credit()
+{
+    backDiv.style.display = 'block'
+    menuDiv.style.display = 'none'
+    creditsDiv.style.display = 'block'
+}
+
+function back()
+{
+    creditsDiv.style.display = 'none'
+    typeDiv.style.display = 'none'
+    menuDiv.style.display = 'block'
+    backDiv.style.display = 'none'
+}
+
+function start()
+{
+    typeDiv.style.display = 'none'
+    menuDiv.style.display = 'none'
+    backDiv.style.display = 'none'
+
+    for(let i = 0; i < 30; i++)
     {
         const buttElement = document.createElement('button')
-        buttElement.textContent = key
-        buttElement.setAttribute('id', key)
         buttElement.addEventListener('click', onCLick)
-        keybord.append(buttElement)  
-    })
+        tileDisplay.append(buttElement) 
+    }
+
+    keys.forEach(key => 
+        {
+            const buttElement = document.createElement('button')
+            buttElement.textContent = key
+            buttElement.setAttribute('id', key)
+            buttElement.addEventListener('click', onCLick)
+            keybord.append(buttElement)  
+        })  
+}
